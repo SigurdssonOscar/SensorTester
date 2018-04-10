1b4105996d6c4845073766029d4916cf464fe798
SigurdssonOscar <dat14osi@student.lu.se>
2018-04-10 13:44:31 +0200

    AddedProject

---
+++ app/src/main/java/com/example/galebre/sensortester/SensorMain.java
@@ -0,0 +1,14 @@
+package com.example.galebre.sensortester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_main);
+        //Code
+    }
+}
