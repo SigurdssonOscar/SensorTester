c517dc3cf5453c126ec6aad310cdda86a770693e
SigurdssonOscar <dat14osi@student.lu.se>
2018-04-11 12:03:45 +0200

    Compass done (maybe broken?)

--- app/src/main/java/com/example/galebre/sensortester/SensorMain.java
+++ app/src/main/java/com/example/galebre/sensortester/SensorMain.java
@@ -1,13 +1,27 @@
 package com.example.galebre.sensortester;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SensorMain extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_main);
+    }
+
+    public void startCompass(View view){
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
+    }
+
+    public void startAccelerometer(View view){
+        Intent intent = new Intent(this, Accelerometer.class);
+        startActivity(intent);
     }
 }
